9ced01f8ed113b641e06faac1f28ce8fd8176e56
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-04-04 23:52:54 +0800

    olives: biometrics: Import FP HIDL from lineage-18.1
    HEAD is at 297cf8975e647eea7a6b75d5ee0ecd5e70880037
    
    Also enable fingerprint stuffs.

--- device.mk
+++ device.mk
@@ -21,10 +21,19 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 TARGET_SCREEN_HEIGHT := 1572
 TARGET_SCREEN_WIDTH := 720
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_sdm439_pm8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_sdm439_pm8953.xml
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_olives \
+    android.hardware.biometrics.fingerprint@2.2
 
 # Sensors
 PRODUCT_COPY_FILES += \
